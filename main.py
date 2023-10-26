9bec40d07e87e1548c0d7ba1808d6f2a8171af43
ale-aleksandra <aleksandra99j@gmail.com>
2023-10-26 15:01:49 +0100

    Find and print results

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def find_highest_bidder(bidding_record):
         if bid_amount > highest_bid:
             highest_bid = bid_amount
             winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}.")
 
 
 while not bidding_finised:
@@ -24,6 +25,7 @@ while not bidding_finised:
     should_continue = input("Are there any other bidders? Type 'yes' or 'no'. ")
     if should_continue == 'no':
         bidding_finised = True
+        find_highest_bidder(bids)
     
 
 
