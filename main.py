4f692177cd2c0637b9a2492ae48670815ed4c55d
ale-aleksandra <aleksandra99j@gmail.com>
2023-10-26 14:46:35 +0100

    Add while loop to check for more bidders

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ from art import logo
 print(logo)
 
 bids = {}
+bidding_finised = False
 
-name = input("What's your name? ")
-price = input("What's your bid? $")
-bids[name] = price
+while not bidding_finised:
+    name = input("What's your name? ")
+    price = input("What's your bid? $")
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no'. ")
+    if should_continue == 'no':
+        bidding_finised = True
+
+
+
