a52b27f71fbe71e7cbfa41974a0cce81cafe34d7
ale-aleksandra <aleksandra99j@gmail.com>
2023-10-26 14:55:50 +0100

    Find the highest bid

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ print(logo)
 bids = {}
 bidding_finised = False
 
+
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+
+
 while not bidding_finised:
     name = input("What's your name? ")
     price = input("What's your bid? $")
@@ -13,6 +22,6 @@ while not bidding_finised:
     should_continue = input("Are there any other bidders? Type 'yes' or 'no'. ")
     if should_continue == 'no':
         bidding_finised = True
-
+    
 
 
