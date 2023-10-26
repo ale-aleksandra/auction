fde455828941cd31e59f6aa5dada3a8ce1b73f1f
ale-aleksandra <aleksandra99j@gmail.com>
2023-10-26 14:59:39 +0100

    Find the highest bidder

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ bidding_finised = False
 
 def find_highest_bidder(bidding_record):
     highest_bid = 0
+    winner = ""
     for bidder in bidding_record:
         bid_amount = bidding_record[bidder]
         if bid_amount > highest_bid:
             highest_bid = bid_amount
+            winner = bidder
 
 
 while not bidding_finised:
